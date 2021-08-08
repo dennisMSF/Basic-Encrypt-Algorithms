ceb1e0ce06ca963d81444ec7b2604b7102524794
dennisMSF <87782818+dennisMSF@users.noreply.github.com>
2021-08-07 22:36:03 -0500

    Create math_fun.h

---
+++ Affin/math_fun.h
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+#include <ctime>
+#include <math.h>
+#include <stdio.h>
+using namespace std;
+int euclides(int, int);
+int euclidesext(int, int);
+int modulo(int, int);
