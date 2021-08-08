4d42aaaf0f4e6f5fb0332915f3f33bb9279819e9
dennisMSF <87782818+dennisMSF@users.noreply.github.com>
2021-08-07 23:01:27 -0500

    Create rsa.h

---
+++ RSA/include/rsa.h
@@ -0,0 +1,24 @@
+#include "math_fun.h"
+class rsa{
+public:
+    rsa(int a);
+    rsa(ZZ,ZZ);
+    string cifrado(string);
+    string descifrado(string);
+    vector<ZZ> precalculo(ZZ,ZZ,ZZ);
+    ZZ restochino(vector<ZZ>,ZZ);
+    ZZ getd();
+    ZZ gete();
+    ZZ getn();
+    ZZ getp();
+    ZZ getq();
+private:
+    ZZ p;
+    ZZ q;
+    ZZ phi;
+    ZZ n;
+    ZZ n_ext;
+    ZZ e_ext;
+    ZZ e;
+    ZZ d;
+};
