5cb2eb260c31250839d01e901b254a9158aa8ce0
dennisMSF <87782818+dennisMSF@users.noreply.github.com>
2021-08-07 22:44:37 -0500

    Create math_fun.h

---
+++ Protocol/math_fun.h
@@ -0,0 +1,16 @@
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <cmath>
+#include <NTL/ZZ.h>
+using namespace NTL;
+using namespace std;
+static string alf="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+int gcd(int, int);
+int gcdext(int, int);
+int inversa(int ,int );
+int modulo(int, int);
+int genera();
+int string_to_int(string);
+string int_to_string(int);
